960fed8964b8c477cd5d74799ef6e7881f3e3e8b
Camille BOUCHACOT <110158288+camillebouchacot@users.noreply.github.com>
2022-09-28 15:36:50 +0200

    Add files via upload

---
+++ front/js/cartforeach.js
@@ -0,0 +1,124 @@
+
+export const displayCart = (productsInlocalStorage) => {
+
+  // J'insère le contenu 
+
+  productsInlocalStorage.forEach(product => {
+
+    const items = document.querySelector("#cart__items");
+    const cartOrder = document.getElementsByClassName("cart__order");
+    console.log(productsInlocalStorage);
+
+
+    fetch('http://localhost:3000/api/products')
+      .then(function (response) {
+
+
+        return response.json()
+      }).then(function (data) {
+        let canap = data.find(x => x._id === product.productId)
+        // console.log(canap);
+        displayitem(product, canap,)
+      }).catch(function (err) {
+        console.warn('something went wrong', err);
+      })
+
+
+
+
+
+    function displayitem(product, canap) {
+      const item =
+
+
+        `<article class="cart__item" data-id='${product.productId}' data-color="${product.option}">
+      <div class="cart__item__img">
+      <img src=${product.imageUrl} alt='${product.altTxt}'>
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${product.name}</h2>
+          <p>${product.option}</p>
+          <p>${canap.price}€</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+          </div>
+          <div class="cart__item__content__settings__delete">
+          
+          <p class="deleteItem" data-id="${product.productId}" data-color="${product.option}">Supprimer</p>
+          </div>
+        </div>
+      </div>
+    </article>`
+      items.insertAdjacentHTML('beforeEnd', item)
+
+      // Suppression des produits
+
+      function deleteItem() {
+        let buttons = document.querySelectorAll('.deleteItem');
+        for (let button of Array.from(buttons)) {
+
+          // Je sélectionne le bouton "supprimer"
+
+          button.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            let id_supprimer = button.getAttribute('data-id')
+            console.log(id_supprimer)
+
+            // avec la méthode filter, je sélectionne les éléements a garder , et supprime l'élément concerné
+            productsInlocalStorage = productsInlocalStorage.filter(el => el.productId !== id_supprimer);
+            console.log(productsInlocalStorage);
+
+            // La formule "removeitem" permet de supprimer completement le tableau products
+            localStorage.removeItem("products");
+
+            // Je met le localstorage au format JSON , pour que le naviguateur suive les instructions
+            localStorage.setItem("products", JSON.stringify(productsInlocalStorage));
+
+
+            // Redirection de la page
+            window.location.href = "cart.html";
+
+          })
+        }
+      }
+
+      deleteItem();
+
+    }
+
+
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
